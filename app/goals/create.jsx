aff846e0dec69b4b54645c5ad46b6535d2e874ab
RoqueDalogdog <roq.dalogdog21@gmail.com>
2025-08-26 08:43:41 +0800

    index and create were changed

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#1253a7ff',
     color: 'white',
     borderRadius: 8,
   }
--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        M Y  G O A L T R A C K E R
+        M Y  G O A L  T R A C K E R
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#ff33d3ff',
+    backgroundColor: '#1253a7ff',
     color: 'white',
     borderRadius: 8,
   },
