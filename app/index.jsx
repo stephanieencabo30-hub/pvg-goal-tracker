e3e5b1935554251bf3faf10b65c85d3d9dce1e96
RoqueDalogdog <roq.dalogdog21@gmail.com>
2025-08-26 08:36:03 +0800

    gi usab nako ang ngalan

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        M Y  G O A L S
+        M Y  G O A L T R A C K E R
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
