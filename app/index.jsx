88b784a23b8bd6780f050211be713d45d2e213a0
RoqueDalogdog <roq.dalogdog21@gmail.com>
2025-08-26 08:26:31 +0800

    first commit

--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#ff33d3ff',
     color: 'white',
     borderRadius: 8,
   },
